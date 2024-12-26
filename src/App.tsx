10344cdd893704cd720ff4235c7aa53439c8dbd9
WooGi1020 <jungwk1020@gmail.com>
2024-12-26 18:11:27 +0900

    feat: 구현 페이지 라우터 등록

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Layout from './pages/Layout';
+import SignInPage from './pages/sign-in';
+import SignUpPage from './pages/sign-up';
 
 function App() {
   const queryClient = new QueryClient();
@@ -10,7 +13,11 @@ function App() {
       <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/">
+            <Route index element={<Layout />} />
+          </Route>
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
