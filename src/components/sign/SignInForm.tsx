f8e55734386c934d549ddfd1f31f57133e481abe
WooGi1020 <jungwk1020@gmail.com>
2024-12-27 16:07:28 +0900

    feat: 비밀번호 체크 로직 추가

--- src/components/sign/SignInForm.tsx
+++ src/components/sign/SignInForm.tsx
@@ -4,8 +4,11 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { signInValidationSchema } from '../../libs/schemas/auth';
 import { SignInTypes } from 'gachTaxi-types';
 import Input from '../commons/Input';
+import { useState } from 'react';
 
 const SignInForm = () => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
   const signInForm = useForm<z.infer<typeof signInValidationSchema>>({
     resolver: zodResolver(signInValidationSchema),
     defaultValues: {
@@ -33,12 +36,19 @@ const SignInForm = () => {
         type="email"
         autoFocus
       />
-      <Input
-        control={signInForm.control}
-        name="password"
-        label="비밀번호"
-        type="password"
-      />
+      <div className="relative flex flex-col gap-2">
+        <Input
+          control={signInForm.control}
+          name="password"
+          label="비밀번호"
+          type={isShowPassword ? 'text' : 'password'}
+        />
+        {/* showPassword 제어할 버튼 아이콘 들어갈 공간 */}
+        <div
+          onClick={() => setIsShowPassword((prev) => !prev)}
+          className="absolute w-5 h-5 bg-slate-500 right-1 top-[35px] cursor-pointer"
+        ></div>
+      </div>
       <button type="submit" className="border mt-6">
         로그인
       </button>
--- src/components/sign/SignUpForm.tsx
+++ src/components/sign/SignUpForm.tsx
@@ -4,8 +4,14 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { signUpValidationSchema } from '../../libs/schemas/auth';
 import { SignUpTypes } from 'gachTaxi-types';
 import Input from '../commons/Input';
+import { useState } from 'react';
 
 const SignUpForm = () => {
+  const [isShowPassword, setIsShowPassword] = useState({
+    password: false,
+    passwordConfirm: false,
+  });
+
   const signUpForm = useForm<z.infer<typeof signUpValidationSchema>>({
     resolver: zodResolver(signUpValidationSchema),
     defaultValues: {
@@ -42,18 +48,39 @@ const SignUpForm = () => {
         label="이메일"
         type="email"
       />
-      <Input
-        control={signUpForm.control}
-        name="password"
-        label="비밀번호"
-        type="password"
-      />
-      <Input
-        control={signUpForm.control}
-        name="passwordConfirm"
-        label="비밀번호 확인"
-        type="password"
-      />
+      <div className="relative flex flex-col gap-2">
+        <Input
+          control={signUpForm.control}
+          name="password"
+          label="비밀번호"
+          type={isShowPassword.password ? 'text' : 'password'}
+        />
+        {/* showPassword 제어할 버튼 아이콘 들어갈 공간 */}
+        <div
+          onClick={() =>
+            setIsShowPassword((prev) => ({ ...prev, password: !prev.password }))
+          }
+          className="absolute w-5 h-5 bg-slate-500 right-1 top-[35px] cursor-pointer"
+        ></div>
+      </div>
+      <div className="relative flex flex-col gap-2">
+        <Input
+          control={signUpForm.control}
+          name="passwordConfirm"
+          label="비밀번호 확인"
+          type={isShowPassword.passwordConfirm ? 'text' : 'password'}
+        />
+        {/* showPassword 제어할 버튼 아이콘 들어갈 공간 */}
+        <div
+          onClick={() =>
+            setIsShowPassword((prev) => ({
+              ...prev,
+              passwordConfirm: !prev.passwordConfirm,
+            }))
+          }
+          className="absolute w-5 h-5 bg-slate-500 right-1 top-[35px] cursor-pointer"
+        ></div>
+      </div>
       <button type="submit" className="border mt-6">
         회원가입
       </button>
