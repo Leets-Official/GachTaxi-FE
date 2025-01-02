bbba6a8bf81bc1cbdb20000862f2276e90b59468
우기 <113000290+WooGi1020@users.noreply.github.com>
2025-01-02 17:57:11 +0900

    style: 테일윈드 확장 변수 설정 (#9)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        header: '20px',
+        captionHeader: '16px',
+        body: '14px',
+        captionBody: '14px',
+        assistive: '12px',
+        button: '16px',
+      },
+      letterSpacing: {
+        header: '-0.05em',
+      },
+      colors: {
+        primary: '#08F283', // Primary 색상
+        secondary: '#05A55A', // Secondary 색상
+        neutral: '#012619', // Neutral 배경 색상
+        addGreen: '#05D976', // 추가 색상 1
+        addDarkGreen: '#02733E', // 추가 색상 2
+        textLightGray: '#D9D9D9', // 텍스트 연회색
+        textDarkGray: '#838383', // 텍스트 진회색
+      },
+      margin: {
+        vertical: '16px',
+        horizontal: '16px',
+      },
+    },
   },
   plugins: [],
 };
