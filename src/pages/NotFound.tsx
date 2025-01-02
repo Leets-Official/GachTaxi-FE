b5a19e8dcbfcbdff26cddb5bb17408ef332f2db7
WooGi1020 <jungwk1020@gmail.com>
2025-01-02 23:41:34 +0900

    feat: 임시 not-found 페이지 추가

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-gray-800">
+        페이지를 찾을 수 없어요!
+      </h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
