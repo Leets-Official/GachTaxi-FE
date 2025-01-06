3a4ee58c2615ad79de79bc6c13a6fa49059b08f4
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-06 21:14:38 +0900

    fix: 로그인 관련 로직 import

--- src/pages/landing/FirstLanding.tsx
+++ src/pages/landing/FirstLanding.tsx
@@ -1,32 +1,15 @@
-import Button from '@/components/commons/Button';
-import Kakao from '@/assets/icon/kakao.svg';
-import Google from '@/assets/icon/google.svg';
-import Header from '@/components/landing/Header';
-import onClickGoogle from '@/components/landing/ClickGoogle';
-import onClickKakao from '@/components/landing/ClickKakao';
-import GachiTaxi from '@/assets/icon/gachiTaxi.svg';
+import LandingHeader from '@/components/landing/LandingHeader';
+import KakaoLoginButton from '@/components/landing/KakaoLoginButton';
+import GachiTaxi from '@/assets/icon/gachiTaxi.svg?react';
+import GoogleLoginButton from '@/components/landing/GoogleLoginButton';
 
 const FirstLanding = () => {
   return (
     <>
-      <Header />
-      <img src={GachiTaxi} alt="gachTaxi" className="float-right mb-[150px]" />
-      <Button
-        variant="primary"
-        className="bg-yellow-300 text-black w-full flex justify-center"
-        onClick={onClickKakao}
-      >
-        <img src={Kakao} alt="kakao" className="mr-2" />
-        카카오로 시작하기
-      </Button>
-      <Button
-        variant="primary"
-        className="bg-white text-black w-full flex justify-center mt-3"
-        onClick={onClickGoogle}
-      >
-        <img src={Google} alt="google" className="mr-2 align-middle" />
-        구글로 시작하기
-      </Button>
+      <LandingHeader />
+      <GachiTaxi className="float-right mb-[70px]" />
+      <KakaoLoginButton />
+      <GoogleLoginButton />
     </>
   );
 };
