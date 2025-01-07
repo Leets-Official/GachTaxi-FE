112f4a5f11c20cb3be45bfa4726d9f4d372aef1e
WooGi1020 <jungwk1020@gmail.com>
2025-01-07 13:39:06 +0900

    feat: oauth 라이브러리 연결 및 로직 작성

--- src/components/landing/GoogleLoginButton.tsx
+++ src/components/landing/GoogleLoginButton.tsx
@@ -1,14 +1,33 @@
+import { useGoogleLogin } from '@react-oauth/google';
 import Button from '../commons/Button';
 import GoogleIcon from '@/assets/icon/google.svg?react';
+import { useNavigate } from 'react-router-dom';
 
 const GoogleLoginButton = () => {
-  const handleGoogleLogin = () => {};
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = useGoogleLogin({
+    onSuccess: (credentialResponse) => {
+      if (credentialResponse) {
+        // 백엔드 API 호출
+        console.log(credentialResponse);
+        navigate('/signup/verification');
+        // if data!.status === "UN_REGISTER"
+        // navigate('/signup/verification');
+        // else
+        // navigate('/dashboard'); path는 미정
+      }
+    },
+    onError: (error) => {
+      throw new Error(`에러 발생 : ${error}`);
+    },
+  });
 
   return (
     <Button
       variant="primary"
-      className="bg-white text-black w-full flex justify-center mt-3"
-      onClick={handleGoogleLogin}
+      className="bg-white text-black w-full flex justify-center mt-3 items-center"
+      onClick={() => handleGoogleLogin()}
     >
       <GoogleIcon className="mr-2 align-middle" />
       구글로 시작하기
