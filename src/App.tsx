07e9ade26c815d3c72c2bd0d06d9b971675375ac
WooGi1020 <jungwk1020@gmail.com>
2025-01-08 17:07:33 +0900

    refactor: 리액트 라우터 레이아웃 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Layout from './pages/Layout';
 import LandingPage from '@/pages/landing/index';
 import SignUpPage from './pages/sign-up';
 import { ModalProvider } from './contexts/ModalContext';
+import HomePage from '@/pages/home';
+import NotFoundPage from '@/pages/NotFound';
+import Layout from '@/pages/Layout';
 
 function App() {
   const queryClient = new QueryClient();
@@ -15,14 +17,11 @@ function App() {
       <BrowserRouter>
         <ModalProvider>
           <Routes>
-            {/* 네비바 포함안됨 */}
-            <Route index element={<LandingPage />} />
-            <Route path="/signup/*" element={<SignUpPage />} />
-
-            {/* 네비바 포함함 */}
-            <Route path="/*" element={<Layout />}>
-              {/* <Route path="dashboard" element={<Dashboard />} />
-            <Route path="profile" element={<Profile />} /> */}
+            <Route path="/" element={<Layout />}>
+              <Route index element={<LandingPage />} />
+              <Route path="/signup/*" element={<SignUpPage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/*" element={<NotFoundPage />} />
             </Route>
           </Routes>
         </ModalProvider>
