ac309090dab1351af85212f39d352b0879b1cace
WooGi1020 <jungwk1020@gmail.com>
2025-01-12 17:28:54 +0900

    feat: 맵 컴포넌트 렌더링

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,8 @@
 import MatchingSheet from '@/components/home/MatchingSheet';
 import Navbar from '@/components/home/Navbar';
-import { useState } from 'react';
+import { useState, Suspense, lazy } from 'react';
+
+const KakaoMap = lazy(() => import('@/components/home/KakaoMap'));
 
 const HomePage = () => {
   const [modalContent, setModalContent] = useState({
@@ -10,7 +12,10 @@ const HomePage = () => {
   });
 
   return (
-    <section className="w-full flex-1 overflow-hidden p-[16px] relative bg-neutral">
+    <section className="w-full flex-1 overflow-hidden relative bg-neutral">
+      <Suspense fallback={<div className="bg-white h-[85vh]">로딩중...</div>}>
+        <KakaoMap />
+      </Suspense>
       <MatchingSheet modalContent={modalContent} />
       <Navbar modalContent={modalContent} setModalContent={setModalContent} />
     </section>
