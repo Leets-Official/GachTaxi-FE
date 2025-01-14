cfe475dd74b0f3fca9f093ab300696ce36ae386e
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-14 14:13:38 +0900

    fix: button_data에서 path와 onclick 구분 처리

--- src/components/my-page/MyPageButton.tsx
+++ src/components/my-page/MyPageButton.tsx
@@ -4,28 +4,38 @@ import { BUTTON_DATA } from '@/constants';
 
 const MyPageButton = () => {
   return (
-    <div className="w-[98%] flex flex-col gap-[25px] font-bold pb-[30px]">
+    <div className="w-[98%] flex flex-col gap-[25px] pb-[30px]">
       {BUTTON_DATA.map((item, index) => (
         <div key={index} className="flex justify-between">
-          <Link
-            to={item.path}
-            className="flex justify-between items-center w-full text-captionHeader"
-          >
-            <span>{item.label}</span>
-            {item.label === '전화번호 인증' ? (
-              <span
-                className={`text-assistive h-[26px] p-4 rounded-full flex items-center justify-center ${
-                  item.isVerified
-                    ? 'bg-primary text-addRed'
-                    : 'bg-addRed text-white'
-                }`}
-              >
-                인증확인
-              </span>
-            ) : (
+          {item.onClick ? (
+            <button
+              onClick={item.onClick}
+              className="flex justify-between items-center w-full text-captionHeader"
+            >
+              <span>{item.label}</span>
               <BackIcon className="rotate-180" />
-            )}
-          </Link>
+            </button>
+          ) : (
+            <Link
+              to={item.path}
+              className="flex justify-between items-center w-full text-captionHeader"
+            >
+              <span>{item.label}</span>
+              {item.label === '전화번호 인증' ? (
+                <span
+                  className={`text-assistive h-[26px] p-4 rounded-full flex items-center justify-center ${
+                    item.isVerified
+                      ? 'bg-primary text-addRed'
+                      : 'bg-addRed text-white'
+                  }`}
+                >
+                  인증확인
+                </span>
+              ) : (
+                <BackIcon className="rotate-180" />
+              )}
+            </Link>
+          )}
         </div>
       ))}
     </div>
