cc10de195394c7dc41797bac701620af82566d83
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-14 22:30:01 +0900

    fix: useNavigate 사용 수정

--- src/components/my-page/LogOutButton.tsx
+++ src/components/my-page/LogOutButton.tsx
@@ -1,7 +1,15 @@
-const onLogout = () => {
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('refreshToken');
-  window.location.href = '/';
+import { useNavigate } from 'react-router-dom';
+
+const LogOutButton: React.FC = () => {
+  const navigate = useNavigate();
+
+  const onLogout = async () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    navigate('/');
+  };
+
+  return <button onClick={onLogout}>로그아웃</button>;
 };
 
-export default onLogout;
+export default LogOutButton;
