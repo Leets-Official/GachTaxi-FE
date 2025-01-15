3c5ff67a13e8a452c2fd7666e5f061af538c7f59
WooGi1020 <jungwk1020@gmail.com>
2025-01-15 20:06:06 +0900

    chore: 상수 파일 핫리로딩 방지

---
+++ src/constants/styles.constant.ts
@@ -0,0 +1,7 @@
+export const GRADIENT_STYLE = {
+  maskImage:
+    'linear-gradient(to bottom, transparent, black 20%, black 80%, transparent)',
+  WebkitMaskImage:
+    'linear-gradient(to bottom, transparent, black 20%, black 80%, transparent)',
+  WebkitMaskComposite: 'destination-in', // Safari 호환
+} as const;
