9ef1687cb6e1aef1984a29687d3f3dfcff7a29fb
WooGi1020 <jungwk1020@gmail.com>
2025-01-16 22:38:53 +0900

    refactor: 최신화된 로직에 따른 form 형태 재구성

--- src/pages/manual-register/index.tsx
+++ src/pages/manual-register/index.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import BackButton from '@/components/commons/BackButton';
 import Button from '@/components/commons/Button';
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+import {
+  useForm,
+  SubmitHandler,
+  Controller,
+  FieldValues,
+} from 'react-hook-form';
 import { ManualMatchingTypes } from 'gachTaxi-types';
 import { manualMatchingSchema } from '@/libs/schemas/match';
 import RouteSetting from '@/components/home/autoMatching/RouteSetting';
@@ -11,27 +17,77 @@ import SelectTags from '@/components/home/autoMatching/selectTags';
 import AddContent from '@/components/manual-register/AddContent';
 import { formatTimeToSelect } from '@/utils';
 import TimeSelect from '@/components/manual-register/timeSelect';
+import useGeoLocation from '@/hooks/useGeoLocation';
+import { useEffect, useCallback } from 'react';
+import getCoordinateByAddress from '@/libs/apis/getCoordinateByAddress';
 
 const ManualMatchingRegister = () => {
   const manualMatchingForm = useForm<z.infer<typeof manualMatchingSchema>>({
     resolver: zodResolver(manualMatchingSchema),
     defaultValues: {
-      route: 'BASIC',
+      startPoint: '',
+      startName: '가천대 정문',
+      destinationPoint: '',
+      destinationName: '가천대 AI 공학관',
       time: formatTimeToSelect(new Date(new Date().setHours(1, 0, 0, 0))),
       members: [],
-      tags: [],
+      criteria: [],
       content: '',
+      expectedTotalCharge: 4800,
     },
   });
+
+  const location = useGeoLocation();
+
+  // 목적지 좌표 설정 함수
+  const updateDestinationCoordinates = useCallback(async () => {
+    try {
+      const address = manualMatchingForm.getValues('destinationName');
+      const coordinates: any = await getCoordinateByAddress(address);
+      manualMatchingForm.setValue(
+        'destinationPoint',
+        `${coordinates.lat}, ${coordinates.lng}`,
+      );
+    } catch (error) {
+      console.error('목적지 좌표 로드 오류', error);
+    }
+  }, [manualMatchingForm]);
+
+  // 위치 정보 업데이트
+  useEffect(() => {
+    if (
+      location.loaded &&
+      location.coordinates.lat &&
+      location.coordinates.lng
+    ) {
+      manualMatchingForm.setValue(
+        'startPoint',
+        `${location.coordinates.lat}, ${location.coordinates.lng}`,
+      );
+    }
+  }, [location, manualMatchingForm]);
+
+  // 목적지 정보 업데이트
+  useEffect(() => {
+    if (window.kakao?.maps) {
+      window.kakao.maps.load(updateDestinationCoordinates);
+    } else {
+      console.error('카카오맵 api 동작 오류');
+    }
+  }, [updateDestinationCoordinates]);
 
   const handleSubmitToManualMatching: SubmitHandler<ManualMatchingTypes> = (
     data,
   ) => {
     try {
-      console.log(data);
+      console.log('Submitted Data:', data);
     } catch (e) {
-      console.error(e);
+      console.error('Submission Error:', e);
     }
+  };
+
+  const handleError = (errors: FieldValues) => {
+    console.error(errors);
   };
 
   return (
@@ -47,11 +103,9 @@ const ManualMatchingRegister = () => {
         <Controller
           name="time"
           control={manualMatchingForm.control}
-          render={({ field }) => {
-            return (
-              <TimeSelect timeVal={field.value} onChange={field.onChange} />
-            );
-          }}
+          render={({ field }) => (
+            <TimeSelect timeVal={field.value} onChange={field.onChange} />
+          )}
         />
         <InviteMembers control={manualMatchingForm.control} />
         <SelectTags control={manualMatchingForm.control} />
@@ -65,7 +119,7 @@ const ManualMatchingRegister = () => {
           className="w-full"
           onClick={manualMatchingForm.handleSubmit(
             handleSubmitToManualMatching,
-            (errors) => console.error('폼 검증 실패:', errors),
+            handleError,
           )}
         >
           매칭등록
