0d4325d37d41e06683adb228de9146a5dbe1fcd2
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-16 21:30:20 +0900

    feat: 카카오 로그인 api 연결

---
+++ src/libs/apis/kakaoLogin.api.ts
@@ -0,0 +1,11 @@
+import { client } from '@/libs/apis/clients';
+
+export const kakaoLogin = async (authCode: string) => {
+  try {
+    const response = await client.post('/auth/login/kakao', { authCode });
+    return response;
+  } catch (error) {
+    console.error('Kakao login API 호출 실패:', error);
+    throw error;
+  }
+};
