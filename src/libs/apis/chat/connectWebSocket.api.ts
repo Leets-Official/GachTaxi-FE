b5649856137b0af1afcdc5e773366dd7e354648b
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-17 11:10:54 +0900

    feat: 웹소켓, 구독 api 연결

--- src/libs/apis/chat/connectWebSocket.api.ts
+++ src/libs/apis/chat/connectWebSocket.api.ts
@@ -1,43 +1,45 @@
-import { Client } from '@stomp/stompjs';
+import { CompatClient, Stomp } from '@stomp/stompjs';
+import { useState, useEffect } from 'react';
 import SockJs from 'sockjs-client';
 
-export const connectToWebSocket = (
-  accessToken: string,
-  senderName: string,
-): Client => {
-  const socketUrl = `${import.meta.env.VITE_API_BASE_URL}/ws`;
-  console.log('socketUrl: ', socketUrl);
+const useWebSocket = (roomId: number) => {
+  const [stompClient, setstompClient] = useState<CompatClient | null>(null);
+  const accessToken = import.meta.env.VITE_MASTER_TOKEN;
 
-  let socket;
-  try {
-    socket = new SockJs(socketUrl);
-    console.log('SockJS socket created:', socket);
-  } catch (error) {
-    console.error('Error creating SockJS socket:', error);
-    throw error;
-  }
+  useEffect(() => {
+    const connectWebSocket = () => {
+      const socket = new SockJs(`${import.meta.env.VITE_API_BASE_URL}/ws`);
+      console.log('socket', socket);
+      const client = Stomp.over(socket);
+      console.log('client', client);
+      client.configure({
+        reconnectDelay: 5000,
+      });
+      client.connect(
+        { Authorization: `Bearer ${accessToken}` },
+        () => {
+          client.subscribe(`/sub/chat/room/${roomId}`, () => {
+            //const receivedMessage = res.data;
+          });
+        },
+        (error: undefined) => {
+          console.log('웹소켓 연결 에러....');
+          console.error('웹소켓 연결 에러:', error);
+          throw error;
+        },
+      );
+      setstompClient(client);
+    };
+    if (roomId && !stompClient) {
+      connectWebSocket();
+    }
 
-  const stompClient = new Client({
-    webSocketFactory: () => socket,
-    debug: (str) => console.log('[STOMP Debug]:', str),
-    reconnectDelay: 5000,
-    onConnect: (frame) => {
-      console.log('WebSocket connected:', frame);
-    },
-    onStompError: (frame) => {
-      console.error('STOMP error:', frame);
-    },
-    onWebSocketError: (error) => {
-      console.error('WebSocket error:', error);
-    },
-  });
-
-  stompClient.connectHeaders = {
-    Authorization: `Bearer ${accessToken}`,
-    senderName: senderName,
-  };
-
-  stompClient.activate();
-
-  return stompClient;
+    return () => {
+      if (stompClient) {
+        stompClient.disconnect();
+        setstompClient(null);
+      }
+    };
+  }, [roomId, stompClient]);
 };
+export default useWebSocket;
