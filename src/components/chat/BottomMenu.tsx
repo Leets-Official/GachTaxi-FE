b0c2d90d9086a32be8febd7c4323bc91c75572aa
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-17 17:21:16 +0900

    fix: 중복 배열 수정

--- src/components/chat/BottomMenu.tsx
+++ src/components/chat/BottomMenu.tsx
@@ -3,25 +3,28 @@ import SendAccount from '@/assets/icon/sendAccount.svg?react';
 import CancelMatching from '@/assets/icon/cancelMatching.svg?react';
 import CloseMatching from '@/assets/icon/closeMatching.svg?react';
 
+const menuItems = [
+  { icon: CallTaxi, label: '택시 호출' },
+  { icon: SendAccount, label: '계좌 전송' },
+  { icon: CloseMatching, label: '매칭 마감' },
+  { icon: CancelMatching, label: '매칭 취소' },
+];
+
 const BottomMenu = () => {
   return (
     <div className="flex justify-around bg-secondary py-4">
-      <div className="flex flex-col items-center">
-        <CallTaxi />
-        <p className="text-captionBody text-textLightGray mt-2">택시 호출</p>
-      </div>
-      <div className="flex flex-col items-center">
-        <SendAccount />
-        <p className="text-captionBody text-textLightGray mt-2">계좌 전송</p>
-      </div>
-      <div className="flex flex-col items-center">
-        <CloseMatching />
-        <p className="text-captionBody text-textLightGray mt-2">매칭 마감</p>
-      </div>
-      <div className="flex flex-col items-center">
-        <CancelMatching />
-        <p className="text-captionBody text-textLightGray mt-2">매칭 취소</p>
-      </div>
+      {menuItems.map((item, index) => (
+        <MenuItem key={index} Icon={item.icon} label={item.label} />
+      ))}
+    </div>
+  );
+};
+
+const MenuItem = ({ Icon, label }: { Icon: React.FC; label: string }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <Icon />
+      <p className="text-captionBody text-textLightGray mt-2">{label}</p>
     </div>
   );
 };
