28f5a6db24a9867e9b9b3d32715e076d5ce9e5b4
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-17 11:10:04 +0900

    fix: 테스트용 pages index 추가

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,22 +1,19 @@
 import ChatRoom from './ChatRoom';
 
 const ChatPage = () => {
-  const accessToken = localStorage.getItem('accessToken');
-  const senderName = '김지원';
-  const roomId = '1';
+  // const accessToken = localStorage.getItem('accessToken');
+  // console.log(accessToken);
+  // const senderName = '김지원';
+  const roomId = 1;
 
-  if (!accessToken) {
-    return <div>Access token is missing. Please log in again.</div>;
-  }
+  // if (!accessToken) {
+  //   return <div>Access token is missing. Please log in again.</div>;
+  // }
 
   return (
     <div>
       <h1>Chat</h1>
-      <ChatRoom
-        accessToken={accessToken}
-        senderName={senderName}
-        roomId={roomId}
-      />
+      <ChatRoom roomId={roomId} />
     </div>
   );
 };
