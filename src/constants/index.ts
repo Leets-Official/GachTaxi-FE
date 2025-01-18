e0f2bf0f27b7471629ccaf2095d6ca79133d37ae
WooGi1020 <jungwk1020@gmail.com>
2025-01-18 17:02:37 +0900

    remove: 불필요 경로 제거

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
 export * from './agreeValues.constant';
 export * from './thresholds.constant';
-export * from './buttonData.constant';
 export * from './routes.constant';
 export * from './timeOptions.constant';
 export * from './styles.constant';
