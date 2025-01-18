1ddeefad359165ec3e22f9a169681a188a124250
WooGi1020 <jungwk1020@gmail.com>
2025-01-18 17:02:25 +0900

    feat: BackButton 컴포넌트 사용

--- src/pages/my-page/index.tsx
+++ src/pages/my-page/index.tsx
@@ -1,6 +1,3 @@
-import Button from '@/components/commons/Button';
-import BackIcon from '@/assets/icon/backIcon.svg?react';
-import { useNavigate } from 'react-router-dom';
 // import Notice from './Notice';
 // import Inquiry from './Inquiry';
 // import Usage from './Usage';
@@ -8,19 +5,12 @@ import { useNavigate } from 'react-router-dom';
 // import PhoneVerification from './PhoneVerification';
 import MyProfileHeader from '@/components/my-page/MyProfileHeader';
 import MyPageButton from '@/components/my-page/MyPageButton';
+import BackButton from '@/components/commons/BackButton';
 
 const MyPage = () => {
-  const nav = useNavigate();
-
-  const handleBack = () => {
-    nav(-1);
-  };
-
   return (
     <section className="flex-1 w-full flex flex-col gap-[32px] p-horizontal">
-      <Button variant="icon" onClick={handleBack}>
-        <BackIcon />
-      </Button>
+      <BackButton />
       <h1 className="text-header font-bold">마이페이지</h1>
 
       <MyProfileHeader />
