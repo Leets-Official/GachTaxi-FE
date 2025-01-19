11ecf706f64ebb73f1edd19d13881c548561db55
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-19 16:19:22 +0900

    fix: 모바일 뷰 input width 수정

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import BottomMenu from './BottomMenu';
+import BottomMenu from './bottomMenu';
 import ChatPlus from '@/assets/icon/chatPlus.svg?react';
 import ChatX from '@/assets/icon/chatX.svg?react';
 import ChatSend from '@/assets/icon/chatSend.svg?react';
@@ -12,8 +12,8 @@ const ChatInput = () => {
   };
 
   return (
-    <>
-      <div className="h-[55px] flex items-center gap-2 p-4">
+    <div className="fixed left-0 right-0 bottom-0 max-w-[430px] w-full h-[64px] mx-auto bg-secondary">
+      <div className="flex justify-between items-center h-full px-4">
         <button
           onClick={toggleMenu}
           className="rounded-full flex items-center justify-center"
@@ -24,7 +24,7 @@ const ChatInput = () => {
         <input
           type="text"
           placeholder="메시지 입력"
-          className="flex-1 text-white bg-[#465443] rounded-full px-4 py-2 outline-none placeholder-black"
+          className="flex-1 text-white bg-[#465443] rounded-full px-4 py-2 outline-none placeholder-black mx-4"
         />
 
         <button className="flex items-center justify-center">
@@ -32,8 +32,12 @@ const ChatInput = () => {
         </button>
       </div>
 
-      {showMenu && <BottomMenu />}
-    </>
+      {showMenu && (
+        <div className="w-full bg-secondary">
+          <BottomMenu />
+        </div>
+      )}
+    </div>
   );
 };
 
