b7b052b6cd8cd5b7e5ee37fb3281926429d69c68
WooGi1020 <jungwk1020@gmail.com>
2025-01-20 19:01:52 +0900

    feat: 친구 목록 페이지 작성

---
+++ src/components/home/FriendList/FriendInfoItem.tsx
@@ -0,0 +1,61 @@
+import BasicProfileIcon from '@/assets/icon/basicProfileIcon.svg?react';
+import Button from '@/components/commons/Button';
+import { Friend } from '@/components/home/FriendList';
+import FriendSendIcon from '@/assets/icon/friendSendIcon.svg?react';
+import FriendSettingIcon from '@/assets/icon/friendSettingIcon.svg?react';
+import { useModal } from '@/contexts/ModalContext';
+import FriendDeleteOrBlack from '@/components/modal/FriendDeleteOrBlack';
+
+interface FriendInfoItemProps {
+  friend: Friend;
+  setCurrentPage: (value: 'FRIEND_LIST' | 'BLACK_LIST') => void;
+}
+
+const FriendInfoItem = ({ friend, setCurrentPage }: FriendInfoItemProps) => {
+  const { openModal } = useModal();
+
+  const handleFriendSetting = () => {
+    if (setCurrentPage) {
+      openModal(
+        <FriendDeleteOrBlack id={friend.id} setCurrentPage={setCurrentPage} />,
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div className="bg-toastColor rounded-box p-vertical flex items-center justify-between h-[101px]">
+      <div className="flex-1 flex items-center gap-[16px]">
+        <div className="flex items-center gap-[10px] cursor-pointer relative rounded-full bg-textDarkGray">
+          {friend.profileImage ? (
+            <img
+              className="w-[48px] h-[48px] rounded-full object-cover"
+              src={friend.profileImage}
+              alt="친구 프로필 이미지"
+            />
+          ) : (
+            <BasicProfileIcon width={48} height={48} />
+          )}
+        </div>
+        <div className="flex flex-col gap-[8px]">
+          <p className="font-semibold text-captionHeader">{friend.nickName}</p>
+          <span className="font-medium text-body text-textDarkGray">
+            {friend.gender === 'MALE' ? '남자' : '여자'}
+          </span>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center gap-[16px]">
+        <Button variant="icon" onClick={handleFriendSetting}>
+          <FriendSettingIcon />
+        </Button>
+        <Button variant="icon">
+          <FriendSendIcon />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default FriendInfoItem;
