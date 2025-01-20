97e068b1aac14f7580018088da07a59163f1f5ee
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-20 17:24:40 +0900

    fix: 상수 분리

---
+++ src/constants/landingText.constant.ts
@@ -0,0 +1,10 @@
+export const LANDING_TEXTS = {
+  FIRST_TITLE: '지금 바로 매칭하기',
+  FIRST_SUBTITLE: ['자동 매칭 기능을 통해', '현재 위치에서 매칭하기'],
+
+  SECOND_TITLE: '매칭 미리 예약하기',
+  SECOND_SUBTITLE: ['수동 매칭 기능을 통해', '매칭을 미리 예약하기'],
+
+  LAST_TITLE: '키워드로 매칭하기',
+  LAST_SUBTITLE: ['키워드 기능을 통해', '매칭 상대 고르기'],
+} as const;
