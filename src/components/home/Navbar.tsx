7a9268b8256389ce8f2affae1e68cd560cc64b91
김지원 <143994508+woneeeee@users.noreply.github.com>
2025-01-20 20:17:11 +0900

    fix: 랜딩페이지 UI 수정 (#61)
    * fix: text center 수정
    
    * fix: 불필요한 css 삭제
    
    * feat: 페이지 swiper 기능 추가
    
    * feat: 마지막 랜딩페이지 UI 구현
    
    * feat: 두번째 랜딩페이지 UI 구현
    
    * feat: 필요한 svg 추가
    
    * feat: 네비바 프로필 이동 수정
    
    * fix: 전체 위치 수정
    
    * fix: 화면 스크롤 제거
    
    * fix: 상수 분리
    
    * fix: 드래그 motion 추가
    
    * fix: LandingHeader 상수 import
    
    * fix: Button 컴포넌트 삭제
    
    * fix: 상수 컴포넌트 사용
    
    * fix: 스크롤 수정
    
    * fix: 스크롤 구현
    
    * fix: 로그인 버튼 위치 수정
    
    * fix: 로그인 버튼 위치 재조정
    
    * fix: 위 아래 스크롤 제거

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -4,6 +4,7 @@ import MatchIcon from '@/assets/icon/matchIcon.svg?react';
 import FriendIcon from '@/assets/icon/friendsIcon.svg?react';
 import NotificationIcon from '@/assets/icon/notificationIcon.svg?react';
 import BasicProfileIcon from '@/assets/icon/basicProfileIcon.svg?react';
+import { Link } from 'react-router-dom';
 
 interface NavbarProps {
   modalContent: {
@@ -76,15 +77,15 @@ const Navbar = ({ modalContent, setModalContent }: NavbarProps) => {
         <NotificationIcon />
         <span className={`text-[10px] text-textDarkGray`}>알림</span>
       </Button>
-      <Button
-        variant="icon"
+      <Link
+        to="/mypage"
         className="flex flex-col items-center justify-center gap-1"
       >
         <div className="flex items-center rounded-full bg-textDarkGray w-[24px] h-[24px]">
           <BasicProfileIcon className="text-[#AEAEAE]" />
         </div>
-        <span className={`text-[10px] text-textDarkGray`}>프로필</span>
-      </Button>
+        <span className="text-[10px] text-textDarkGray">프로필</span>
+      </Link>
     </nav>
   );
 };
