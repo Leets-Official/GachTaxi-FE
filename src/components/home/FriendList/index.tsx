0186b88af66e617b546707a7547a9d9f31d48e36
WooGi1020 <jungwk1020@gmail.com>
2025-01-20 19:01:39 +0900

    feat: 친구 관련 페이지 작성

---
+++ src/components/home/FriendList/index.tsx
@@ -0,0 +1,97 @@
+import Button from '@/components/commons/Button';
+import BlackListPage from '@/components/home/FriendList/BlackListPage';
+import FriendListPage from '@/components/home/FriendList/FriendListPage';
+import { useState } from 'react';
+
+export interface Friend {
+  id: number;
+  profileImage: string;
+  nickName: string;
+  gender: 'MALE' | 'FEMALE';
+}
+
+const FriendList = ({ isOpen }: { isOpen: boolean }) => {
+  const [friendList, setFriendList] = useState<Friend[]>([
+    {
+      id: 1,
+      profileImage:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9aeHFWXHfDhJ_xzPneJlEsKVdu-tZ5vGoow&s',
+      nickName: '택시보이',
+      gender: 'MALE',
+    },
+    {
+      id: 2,
+      profileImage: '',
+      nickName: '택시걸',
+      gender: 'FEMALE',
+    },
+  ]);
+
+  const [blackList, setBlackList] = useState<Friend[]>([
+    {
+      id: 1,
+      profileImage:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9aeHFWXHfDhJ_xzPneJlEsKVdu-tZ5vGoow&s',
+      nickName: '택시보이',
+      gender: 'MALE',
+    },
+    {
+      id: 2,
+      profileImage: '',
+      nickName: '택시걸',
+      gender: 'FEMALE',
+    },
+  ]);
+
+  const [currentPage, setCurrentPage] = useState<'FRIEND_LIST' | 'BLACK_LIST'>(
+    'FRIEND_LIST',
+  );
+
+  const handlePageChange = () => {
+    setCurrentPage((prev) =>
+      prev === 'FRIEND_LIST' ? 'BLACK_LIST' : 'FRIEND_LIST',
+    );
+  };
+
+  const renderConditionalComponents = () => {
+    if (currentPage === 'FRIEND_LIST') {
+      return (
+        <FriendListPage
+          friendList={friendList}
+          setFriendList={setFriendList}
+          isOpen={isOpen}
+          setCurrentPage={setCurrentPage}
+        />
+      );
+    } else {
+      return (
+        <BlackListPage
+          blackList={blackList}
+          setBlackList={setBlackList}
+          isOpen={isOpen}
+        />
+      );
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-[32px] justify-between relative">
+      <div className="flex items-center justify-start gap-3">
+        <h2 className="text-header font-bold">
+          {currentPage === 'FRIEND_LIST' ? '친구 목록' : '블랙리스트'}
+        </h2>
+        <Button
+          variant="icon"
+          className="underline text-textDarkGray"
+          onClick={handlePageChange}
+        >
+          {currentPage === 'FRIEND_LIST' ? '블랙리스트' : '친구 목록'}
+        </Button>
+      </div>
+
+      {renderConditionalComponents()}
+    </div>
+  );
+};
+
+export default FriendList;
