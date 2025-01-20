0b377310253cbfe47f2b67f98fbdace46b365767
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-20 14:08:16 +0900

    feat: 페이지 swiper 기능 추가

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,11 +1,84 @@
-//추후에 두번째, 세번째 슬라이드 디자인이 구현될 경우 추가할 예정
-import FirstLanding from '@/pages/landing/FirstLanding';
+import { useState, useRef, useEffect } from 'react';
+import FirstLanding from './FirstLanding';
+import SecondLanding from './SecondLanding';
+import LastLanding from './LastLanding';
+import KakaoLoginButton from '@/components/landing/KakaoLoginButton';
+import GoogleLoginButton from '@/components/landing/GoogleLoginButton';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const slides = [<FirstLanding />, <SecondLanding />, <LastLanding />];
 
 const LandingPage = () => {
+  const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  const handleScroll = () => {
+    if (sliderRef.current) {
+      const scrollLeft = sliderRef.current.scrollLeft;
+      const slideWidth = sliderRef.current.clientWidth;
+      const newIndex = Math.round(scrollLeft / slideWidth);
+      setCurrentIndex(newIndex);
+    }
+  };
+
+  const goToSlide = (index: number) => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollTo({
+        left: index * sliderRef.current.clientWidth,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  useEffect(() => {
+    const slider = sliderRef.current;
+    if (slider) {
+      slider.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      if (slider) {
+        slider.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, []);
+
   return (
-    <section className="flex-1 w-full p-horizontal">
-      <FirstLanding />
-    </section>
+    <div className="relative w-full h-screen flex flex-col items-center justify-center overflow-hidden">
+      <div
+        ref={sliderRef}
+        className="mt-[10%] flex w-full h-full overflow-x-auto snap-mandatory snap-x scroll-smooth scroll-hidden"
+      >
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className="min-w-full flex justify-center items-center snap-center"
+          >
+            {slide}
+          </div>
+        ))}
+      </div>
+
+      <div className="absolute bottom-[25%] flex justify-center gap-2 z-30">
+        {slides.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => goToSlide(index)}
+            className={`w-3 h-3 rounded-full cursor-pointer transition-all ${
+              index === currentIndex ? 'bg-primary' : 'bg-textDarkGray'
+            }`}
+          ></div>
+        ))}
+      </div>
+
+      <div className="absolute w-[90%] bottom-10 flex flex-col gap-1 w-full max-w-[430px]">
+        <KakaoLoginButton />
+        <GoogleOAuthProvider clientId={CLIENT_ID}>
+          <GoogleLoginButton />
+        </GoogleOAuthProvider>
+      </div>
+    </div>
   );
 };
 
