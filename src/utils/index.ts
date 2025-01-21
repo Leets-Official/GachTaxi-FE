1057a5ef5de462084d1705c982f359181441a94d
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-21 22:30:45 +0900

    remove: utils 삭제된 파일 제거

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
 export { default as formatTimeLikeTimer } from './formatTimeLikeTimer';
 export { default as formatTimeToSelect } from './formatTimeToSelect';
 export { default as handleDragEnd } from './handleDragEnd';
-export { default as useLogout } from './useLogout';
-export { default as useWithdraw } from './useWithDraw';
