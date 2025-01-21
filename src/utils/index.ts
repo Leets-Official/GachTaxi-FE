256f5798d63fddae2496166fffa4192e26195eeb
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-21 21:42:46 +0900

    feat: 로그아웃, 탈퇴하기 import

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export { default as formatTimeLikeTimer } from './formatTimeLikeTimer';
 export { default as formatTimeToSelect } from './formatTimeToSelect';
 export { default as handleDragEnd } from './handleDragEnd';
+export { default as useLogout } from './useLogout';
+export { default as useWithdraw } from './useWithDraw';
