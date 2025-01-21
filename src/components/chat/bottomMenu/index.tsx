062b3ff16c62bbe1ad125a2e883e989e57137257
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-21 18:13:27 +0900

    feat: 계좌 전송 함수 추가

--- src/components/chat/bottomMenu/index.tsx
+++ src/components/chat/bottomMenu/index.tsx
@@ -1,12 +1,41 @@
 import MenuItem from './MenuItem';
 import { MENUITEMS } from '@/constants';
+import { useState } from 'react';
+import SendAccountModal from '../modal/sendAccountModal';
 
-const BottomMenu = () => {
+const BottomMenu = ({
+  onSendAccount,
+}: {
+  onSendAccount: (account: string) => void;
+}) => {
+  const [showAccountModal, setShowAccountModal] = useState(false);
+
+  const handleSendClick = () => {
+    setShowAccountModal(true);
+  };
+
   return (
     <div className="flex justify-evenly py-8">
       {MENUITEMS.map((item, index) => (
-        <MenuItem key={index} Icon={item.icon} label={item.label} />
+        <div
+          key={index}
+          onClick={item.label === '계좌 전송' ? handleSendClick : undefined}
+          className="cursor-pointer"
+        >
+          <MenuItem key={index} Icon={item.icon} label={item.label} />
+        </div>
       ))}
+
+      {showAccountModal && (
+        <SendAccountModal
+          onClose={() => setShowAccountModal(false)}
+          account="농협 302 XXXX XXXX XX"
+          onSend={(accountInfo) => {
+            onSendAccount(accountInfo);
+            setShowAccountModal(false);
+          }}
+        />
+      )}
     </div>
   );
 };
