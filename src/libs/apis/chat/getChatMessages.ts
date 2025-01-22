e2ccee196ec1e6974e82c405e1d2abb66e13b30f
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-22 20:36:38 +0900

    feat: 채팅방 메시지 조회 api 연결

--- src/libs/apis/chat/getChatMessages.ts
+++ src/libs/apis/chat/getChatMessages.ts
@@ -8,7 +8,7 @@ export const getChatMessages = async (roomId: number | null) => {
 
   try {
     const res = await axios.get(
-      `https://6140-2406-5900-102a-70c2-6568-cdf4-9ae9-b838.ngrok-free.app/api/chat/${roomId}`,
+      `${import.meta.env.VITE_API_BASE_URL}/api/chat/${roomId}`,
     );
     console.log('res:', res.data);
     return res.data;
