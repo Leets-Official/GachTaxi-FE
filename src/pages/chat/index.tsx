27c100ba5fe5f17c57088f59eea2693eab3cda46
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-22 20:31:20 +0900

    fix: roomId props 추가

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -2,9 +2,13 @@ import ChatInput from '@/components/chat/MessageInput';
 import MessageList from '@/components/chat/messageList/index';
 import NewMessage from '@/components/chat/NewMessage';
 import BackButton from '@/components/commons/BackButton';
+import useWebSocket from '@/libs/apis/chat/connectWebSocket.api';
 
 const ChatPage = () => {
   const chatMember = 3;
+  const roomId = 1;
+
+  useWebSocket(roomId);
 
   return (
     <section className="flex-1 w-full flex flex-col gap-[32px] p-horizontal bg-darkBlack">
@@ -18,13 +22,13 @@ const ChatPage = () => {
         </div>
       </div>
       <div className="flex-1 overflow-y-auto">
-        <MessageList />
+        <MessageList roomId={roomId} />
         <div className="w-full flex items-center justify-center">
           <NewMessage />
         </div>
       </div>
       <div className="sticky">
-        <ChatInput />
+        <ChatInput roomId={roomId} />
       </div>
     </section>
   );
