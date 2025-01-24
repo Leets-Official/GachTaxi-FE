225b40ad03270bfcce2c95651badfa61fc49b97f
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-25 00:12:28 +0900

    feat: 전체 메시지 조회 api 연결

--- src/libs/apis/chat/getChatMessages.ts
+++ src/libs/apis/chat/getChatMessages.ts
@@ -1,17 +1,32 @@
 import axios from 'axios';
-//import client from '../clients';
+// import client from '../clients';
 
-export const getChatMessages = async (roomId: number | null) => {
-  if (!roomId) {
-    throw new Error('roomId is required');
-  }
+export const getChatMessages = async (
+  roomId: number | null,
+  lastMessageTimeStamp: string | null = null,
+  pageNumber: number = 0,
+  pageSize: number = 5,
+): Promise<ChatMessagesFromServerFull> => {
+  const accessToken = import.meta.env.VITE_MASTER_TOKEN;
 
   try {
     const res = await axios.get(
       `${import.meta.env.VITE_API_BASE_URL}/api/chat/${roomId}`,
+      {
+        headers: {
+          'ngrok-skip-browser-warning': '69420',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          lastMessageTimeStamp,
+          pageNumber,
+          pageSize,
+        },
+      },
     );
-    console.log('res:', res.data);
-    return res.data;
+    const data = res.data.data;
+    console.log('getChatMessage에서의 data', data);
+    return data;
   } catch (error) {
     throw new Error(`Error get ChatMessage: ${error}`);
   }
