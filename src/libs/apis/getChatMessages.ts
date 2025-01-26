270c2764644adde6d6bf4e69c7e1717c6a5cb2f4
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-26 22:01:29 +0900

    fix: 채팅 메시지 조회

--- src/libs/apis/getChatMessages.ts
+++ src/libs/apis/getChatMessages.ts
@@ -1,15 +1,14 @@
 import axios from 'axios';
 // import client from '../clients';
 
+const accessToken = import.meta.env.VITE_MASTER_TOKEN;
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
 export const getChatMessages = async (
   roomId: number | null,
   lastMessageTimeStamp: string | null = null,
   pageNumber: number = 0,
-  pageSize: number = 5,
 ): Promise<ChatMessagesFromServerFull> => {
-  const accessToken = import.meta.env.VITE_MASTER_TOKEN;
-  const baseUrl = import.meta.env.VITE_API_BASE_URL;
-
   try {
     const res = await axios.get(`${baseUrl}/api/chat/${roomId}`, {
       headers: {
@@ -18,11 +17,12 @@ export const getChatMessages = async (
       params: {
         lastMessageTimeStamp,
         pageNumber,
-        pageSize,
+        pageSize: 1000,
       },
     });
     const data = res.data.data;
     console.log('getChatMessage에서의 data', data);
+    console.log('roomId', roomId);
     return data;
   } catch (error) {
     throw new Error(`Error get ChatMessage: ${error}`);
