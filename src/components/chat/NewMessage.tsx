83d2a1c4b80855433f2f413696074511189692bb
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-26 22:02:07 +0900

    fix: css 수정

--- src/components/chat/NewMessage.tsx
+++ src/components/chat/NewMessage.tsx
@@ -1,6 +1,6 @@
 const NewMessage = () => {
   return (
-    <div className="flex items-center w-[95%] text-textDarkGray mb-2">
+    <div className="flex items-center w-[95%] text-textDarkGray mb-2 justify-center">
       <div className="flex-1 border-t border-textDarkGray"></div>
       <span className="px-4 text-assistive">New Messages</span>
       <div className="flex-1 border-t border-textDarkGray"></div>
