d1d25afbeefc71fd95b4506f7c54bbf4991e62ef
WooGi1020 <jungwk1020@gmail.com>
2025-01-27 18:45:25 +0900

    feat: 이미지 업로드 훅 로직 변경

--- src/hooks/useUploadImage.ts
+++ src/hooks/useUploadImage.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import getImageUrl from '@/libs/apis/auth/getImageUrl.api';
+import getPresignedUrl from '@/libs/apis/auth/getPresignedUrl';
 import { useState, useEffect } from 'react';
 
 const useUploadImage = (image?: any) => {
@@ -25,8 +26,12 @@ const useUploadImage = (image?: any) => {
         const { data, error } = await getImageUrl(image);
         if (error || !data) {
           throw new Error('이미지 업로드 실패');
+        } else {
+          const res = await getPresignedUrl(data, image);
+          const slicedData = res.data.split('?')[0];
+          setUploadedImage(slicedData);
         }
-        setUploadedImage(data);
+
         const objectURL = URL.createObjectURL(image);
         setImagePreview(objectURL);
 
