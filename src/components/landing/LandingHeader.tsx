e2619c032075134b34d72dd21b9651dd63bc5915
WooGi1020 <jungwk1020@gmail.com>
2025-01-28 17:35:55 +0900

    fix: p 태그 중첩사용 제거

--- src/components/landing/LandingHeader.tsx
+++ src/components/landing/LandingHeader.tsx
@@ -7,7 +7,7 @@ const Header = ({ title, subtitle }: HeaderProps) => {
   return (
     <div className="items-center mt-[15%] h-[82px]">
       <h1 className="text-[24px] font-bold mb-[8px]">{title}</h1>
-      <p className="text-body text-textLightGray">{subtitle}</p>
+      <div className="text-body text-textLightGray">{subtitle}</div>
     </div>
   );
 };
