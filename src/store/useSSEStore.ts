ac2b7feb8c25e1cc608af92f26029f23b8fb62b3
WooGi1020 <jungwk1020@gmail.com>
2025-01-29 19:57:01 +0900

    feat: 전역 스토어 로직 개선

--- src/store/useSSEStore.ts
+++ src/store/useSSEStore.ts
@@ -1,5 +1,5 @@
 import { EventSourcePolyfill } from '@/utils/EventSourcePolyfill';
-import { MessagesArray } from 'gachTaxi-types';
+import { MatchingEvent, MessagesArray } from 'gachTaxi-types';
 import { create } from 'zustand';
 
 interface SSEState {
@@ -11,46 +11,59 @@ interface SSEState {
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-const useSSEStore = create<SSEState>((set) => ({
+const useSSEStore = create<SSEState>((set, get) => ({
   sse: null,
   messages: [],
   initializeSSE: () => {
-    set((state) => {
-      if (state.sse) {
-        console.log('이미 구독 중이므로 재구독을 방지합니다.');
-        return state;
-      }
+    const accessToken = localStorage.getItem('accessToken');
 
-      const accessToken = localStorage.getItem('accessToken');
-      const sse = new EventSourcePolyfill(
-        `${BASE_URL}/api/matching/auto/subscribe`,
-        {
-          headers: { Authorization: `Bearer ${accessToken}` },
-          withCredentials: true,
-        },
-      );
+    if (!accessToken) {
+      console.error('엑세스 토큰이 없습니다!');
+      return;
+    }
 
-      sse.onmessage = (event: MessageEvent) => {
-        const formatedData = JSON.parse(event.data.split('data:')[1].trim());
-        set((state) => ({
-          messages: [...state.messages, formatedData],
-        }));
-      };
+    if (get().sse) {
+      console.log('이미 구독 중이므로 재구독을 방지합니다.');
+      return;
+    }
 
-      sse.onerror = (error) => {
-        console.error('SSE 에러 발생:', error);
-        sse.close();
-      };
+    const sse = new EventSourcePolyfill(
+      `${BASE_URL}/api/matching/auto/subscribe`,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+        withCredentials: true,
+      },
+    );
 
-      console.log('SSE 구독 시작');
-      return { sse };
-    });
+    sse.onmessage = (event: MessageEvent) => {
+      const rawData = event.data;
+      const formatedData: MatchingEvent = rawData.startsWith('data:')
+        ? JSON.parse(rawData.split('data:')[1].trim())
+        : JSON.parse(rawData);
+
+      set((state) => {
+        return { messages: [...state.messages, formatedData] };
+      });
+    };
+
+    sse.onerror = (error) => {
+      console.error('SSE 에러 발생:', error);
+      sse.close();
+
+      setTimeout(() => {
+        console.log('SSE 재연결 시도');
+        get().initializeSSE();
+      }, 5000);
+    };
+
+    console.log('SSE 구독 시작');
+    return { sse };
   },
 
   closeSSE: () => {
     set((state) => {
       state.sse?.close();
-      return { sse: null };
+      return { sse: null, messages: [] };
     });
   },
 }));
