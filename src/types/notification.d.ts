060bd6f02c693fddeac6d55dc542007d2d613e38
WooGi1020 <jungwk1020@gmail.com>
2025-01-29 21:29:20 +0900

    fix: 변경된 타입 적용

--- src/types/notification.d.ts
+++ src/types/notification.d.ts
@@ -1,66 +1,27 @@
 declare module '@gachTaxi-types' {
-  interface UnreadNotificationTypes {
-    code: number;
-    message: string;
-    data: {
-      unreadCount: number;
-      hasUnreadNotifications: boolean;
-    };
-  }
-
-  // NotificationResponse 종속
-  type NotificationType =
-    | 'CHAT'
-    | 'MATCH_SUCCESS'
-    | 'MATCH_FAILURE'
-    | 'MATCH_FINISH'
-    | 'FRIEND_REQUEST';
-
-  // NotificationResponse 종속
-  type NotificationStatus = 'UNREAD' | 'READ' | 'UNSENT';
-
-  // NotificationResponse 종속
   interface Notification {
     notificationId: number;
     senderId: number;
     receiverId: number;
-    type: NotificationType;
-    status: NotificationStatus;
+    type: 'MATCH_START' | 'MATCH_FINISH' | 'FRIEND_REQUEST';
     title: string;
     content: string;
     createdAt: string;
   }
 
-  // NotificationResponse 종속
-  interface Sort {
-    empty: boolean;
-    sorted: boolean;
-    unsorted: boolean;
-  }
-
-  // NotificationResponse 종속
   interface Pageable {
-    offset: number;
-    sort: Sort;
-    paged: boolean;
     pageNumber: number;
     pageSize: number;
-    unpaged: boolean;
+    numberOfElements: number;
+    last: boolean;
   }
 
   interface NotificationResponse {
     code: number;
     message: string;
     data: {
-      size: number;
-      content: Notification[];
-      number: number;
-      sort: Sort;
-      first: boolean;
+      response: Notification[];
       pageable: Pageable;
-      numberOfElements: number;
-      last: boolean;
-      empty: boolean;
     };
   }
 }
