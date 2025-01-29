f2092f50471000bb3b88b584409cc8a39be701be
WooGi1020 <jungwk1020@gmail.com>
2025-01-29 21:30:40 +0900

    fix: 타입 변경에 따른 로직 수정

--- src/components/notification/index.tsx
+++ src/components/notification/index.tsx
@@ -1,17 +1,22 @@
 import EmptyView from '@/components/emptyView';
 import FriendRequestNotification from '@/components/notification/FriendRequestNotification';
+import MatchingNotification from '@/components/notification/MatchingNotification';
 import useNotification from '@/hooks/queries/useNotification';
 
 const NotificationList = () => {
   const { data } = useNotification();
-  const notificationList = data.pages.flatMap((page) => page.content);
+  const notificationList = data?.pages?.flatMap((page) => page.response) || [];
 
   return (
     <div className="flex flex-col gap-4 w-full h-full flex-1">
       {notificationList.length > 0 ? (
-        notificationList.map((noti) => {
-          return <FriendRequestNotification />;
-        })
+        notificationList.map((notification) =>
+          notification.type === 'FRIEND_REQUEST' ? (
+            <FriendRequestNotification key={notification.notificationId} />
+          ) : (
+            <MatchingNotification key={notification.notificationId} />
+          ),
+        )
       ) : (
         <EmptyView>알림이 없습니다.</EmptyView>
       )}
