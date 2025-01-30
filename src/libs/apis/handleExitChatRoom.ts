dc85d7c0ff4417f648fc033312ccde85cec6f6ad
김지원 <jiwonkim0531@gachon.ac.kr>
2025-01-30 13:27:06 +0900

    fix: 불필요한 로그 주석 삭제

--- src/libs/apis/handleExitChatRoom.ts
+++ src/libs/apis/handleExitChatRoom.ts
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
-const accessToken = import.meta.env.VITE_MASTER_TOKEN;
+const accessToken = localStorage.getItem('accessToken');
 
 const handleExitChatRoom = async (roomId: number) => {
-  console.log('퇴장 룸넘버', roomId);
   try {
     const response = await axios.delete(`${baseUrl}/api/chat/${roomId}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    console.log('퇴장 처리', response.data);
     const res = response.data;
     return res;
   } catch (error) {
