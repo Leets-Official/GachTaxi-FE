cb7f195ae5540af7cd422b31d9d25478726e0da8
WooGi1020 <jungwk1020@gmail.com>
2025-02-01 16:54:36 +0900

    feat: 훅 사용 및 로딩 설정

--- src/components/notification/index.tsx
+++ src/components/notification/index.tsx
@@ -2,11 +2,21 @@ import EmptyView from '@/components/emptyView';
 import FriendRequestNotification from '@/components/notification/FriendRequestNotification';
 import MatchingNotification from '@/components/notification/MatchingNotification';
 import useNotification from '@/hooks/queries/useNotification';
+import { useIntersectionObserver } from '@/store/useIntersectionObserver';
+import SpinnerIcon from '@/assets/icon/spinnerIcon.svg?react';
 import { AnimatePresence } from 'framer-motion';
 
 const NotificationList = () => {
-  const { data } = useNotification();
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useNotification();
   const notificationList = data?.pages?.flatMap((page) => page.response) || [];
+  const { isIntersecting, ref } = useIntersectionObserver({
+    threshold: 0.5,
+  });
+
+  if (isIntersecting && hasNextPage && !isFetchingNextPage) {
+    fetchNextPage();
+  }
 
   return (
     <div className="flex flex-col gap-4 w-full h-full flex-1">
@@ -34,6 +44,10 @@ const NotificationList = () => {
           <EmptyView>알림이 없습니다.</EmptyView>
         )}
       </AnimatePresence>
+      {isFetchingNextPage && (
+        <SpinnerIcon width={36} height={36} className="mx-auto spinner mt-5" />
+      )}
+      <div ref={ref}></div>
     </div>
   );
 };
--- src/pages/notification/index.tsx
+++ src/pages/notification/index.tsx
@@ -1,5 +1,6 @@
 import BackButton from '@/components/commons/BackButton';
 import NotificationList from '@/components/notification';
+import SpinnerIcon from '@/assets/icon/spinnerIcon.svg?react';
 import { Suspense } from 'react';
 
 const NotificationPage = () => {
@@ -7,7 +8,14 @@ const NotificationPage = () => {
     <section className="flex-1 w-full flex flex-col gap-[32px] p-horizontal h-full">
       <BackButton />
       <h1 className="text-header font-bold">알림</h1>
-      <Suspense fallback={<>로딩중...</>}>
+
+      <Suspense
+        fallback={
+          <div className="h-full w-full flex items-center justify-center flex-1">
+            <SpinnerIcon width={36} height={36} className="mx-auto spinner" />
+          </div>
+        }
+      >
         <NotificationList />
       </Suspense>
     </section>
