e78e84b4111e4ec4b95dc7fa2640eae190e23596
WooGi1020 <jungwk1020@gmail.com>
2025-02-01 16:22:41 +0900

    feat: 삭제 애니메이션 구현

--- src/components/notification/index.tsx
+++ src/components/notification/index.tsx
@@ -2,6 +2,7 @@ import EmptyView from '@/components/emptyView';
 import FriendRequestNotification from '@/components/notification/FriendRequestNotification';
 import MatchingNotification from '@/components/notification/MatchingNotification';
 import useNotification from '@/hooks/queries/useNotification';
+import { AnimatePresence } from 'framer-motion';
 
 const NotificationList = () => {
   const { data } = useNotification();
@@ -9,17 +10,30 @@ const NotificationList = () => {
 
   return (
     <div className="flex flex-col gap-4 w-full h-full flex-1">
-      {notificationList.length > 0 ? (
-        notificationList.map((notification) =>
-          notification.type === 'FRIEND_REQUEST' ? (
-            <FriendRequestNotification key={notification.notificationId} />
-          ) : (
-            <MatchingNotification key={notification.notificationId} />
-          ),
-        )
-      ) : (
-        <EmptyView>알림이 없습니다.</EmptyView>
-      )}
+      <AnimatePresence>
+        {notificationList.length > 0 ? (
+          notificationList.map((notification) =>
+            notification.type === 'FRIEND_REQUEST' ? (
+              <FriendRequestNotification
+                key={notification.notificationId}
+                notificationId={notification.notificationId}
+                senderId={notification.payload.senderId}
+                content={notification.content}
+              />
+            ) : (
+              <MatchingNotification
+                key={notification.notificationId}
+                notificationId={notification.notificationId}
+                content={notification.content}
+                createdAt={notification.createdAt}
+                payload={notification.payload}
+              />
+            ),
+          )
+        ) : (
+          <EmptyView>알림이 없습니다.</EmptyView>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
