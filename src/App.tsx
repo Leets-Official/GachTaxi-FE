7c823e4192a7f2b15b6a0aca65c7a2fc06b8e705
김지원 <jiwonkim0531@gachon.ac.kr>
2025-02-02 22:54:50 +0900

    feat: route 수정

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import FriendRequestPage from '@/pages/friend-request';
 import NotificationPage from '@/pages/notification';
 import ProtectRoute from '@/pages/ProtectRoute';
 import GoogleLoginLoading from '@/components/landing/GoogleLoginLoading';
+import CallTaxi from '@/pages/chat/CallTaxi';
 
 function App() {
   const queryClient = new QueryClient();
@@ -92,6 +93,14 @@ function App() {
                   element={
                     <ProtectRoute isPrivate>
                       <ChatPage />
+                    </ProtectRoute>
+                  }
+                />
+                <Route
+                  path="call-taxi"
+                  element={
+                    <ProtectRoute isPrivate>
+                      <CallTaxi />
                     </ProtectRoute>
                   }
                 />
