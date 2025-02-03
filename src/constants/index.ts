fb4eb66384056440049157e358f8c6a71d0b149a
WooGi1020 <jungwk1020@gmail.com>
2025-02-03 22:43:42 +0900

    feat: tag 상수배열 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,4 @@ export * from './timeOptions.constant';
 export * from './styles.constant';
 export * from './menuItems.constant';
 export * from './landingText.constant';
+export * from './tags.constant';
