af6517995cc2c0d1d52a58b5cdd43a24bfaad4b4
김지원 <143994508+woneeeee@users.noreply.github.com>
2025-02-04 11:54:56 +0900

    Feat: 마이페이지 수정 (#111)
    * feat: 프로필 수정 api 연결
    
    * fix: 계좌번호 type 수정
    
    * fix: 계좌번호 input 추가
    
    * fix: rounded 수정
    
    * fix: profileEditVerificationTypes 설정
    
    * fix: 프로필 수정 로직 추가
    
    * feat: 닉네임 수정 스키마 추가
    
    * fix: profileImageUpload FieldValues 추가
    
    * feat: 프로필 수정 api 연결
    
    * remove: 불필요한 주석 삭제
    
    * fix: 채팅방 계좌 설정
    
    * remove: 불필요한 주석 제거

--- src/pages/my-page/EditProfilePage.tsx
+++ src/pages/my-page/EditProfilePage.tsx
@@ -2,22 +2,24 @@ import BackButton from '@/components/commons/BackButton';
 import Button from '@/components/commons/Button';
 import Input from '@/components/commons/Input';
 import { z } from 'zod';
-import { userInfoVerificationSchema } from '@/libs/schemas/auth';
-import { UserInfoVerificationTypes } from 'gachTaxi-types';
+import { profileEditVerificationSchema } from '@/libs/schemas/auth';
+import { ProfileEditVerificationTypes } from 'gachTaxi-types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import ProfileImageUpload from '@/components/sign/userInfoVerification/ProfileImageUpload';
 import useUploadImage from '@/hooks/useUploadImage';
+import { updateUserProfile } from '@/libs/apis/updateUserProfile.api';
+import { useToast } from '@/contexts/ToastContext';
+import useUserStore from '@/store/useUserStore';
+import handleAxiosError from '@/libs/apis/axiosError.api';
 
 const EditProfilePage = () => {
-  const profileForm = useForm<z.infer<typeof userInfoVerificationSchema>>({
-    resolver: zodResolver(userInfoVerificationSchema),
+  const profileForm = useForm<z.infer<typeof profileEditVerificationSchema>>({
+    resolver: zodResolver(profileEditVerificationSchema),
     defaultValues: {
-      nickname: '',
-      realName: '',
-      studentNumber: '',
-      gender: 'MALE',
+      nickName: '',
       profilePicture: undefined,
+      accountNumber: '',
     },
     mode: 'onSubmit',
   });
@@ -25,16 +27,39 @@ const EditProfilePage = () => {
   const currentImage = profileForm.watch('profilePicture');
   const { imagePreview, uploadedImage, setImagePreview } =
     useUploadImage(currentImage);
+  const { setUser } = useUserStore();
+  const { openToast } = useToast();
 
-  const handleSubmitChange: SubmitHandler<UserInfoVerificationTypes> = (
-    data,
-  ) => {
+  const handleSubmitChange: SubmitHandler<
+    ProfileEditVerificationTypes
+  > = async (data) => {
     try {
-      console.log(data);
-      console.log(uploadedImage);
-      profileForm.setValue('nickname', '');
-    } catch (e) {
-      console.error(e);
+      const updateData = profileForm.getValues();
+      if (
+        data.profilePicture !== uploadedImage &&
+        typeof data.profilePicture !== 'string'
+      ) {
+        updateData.profilePicture = uploadedImage;
+        const res = await updateUserProfile(updateData);
+        if (res?.code === 200) {
+          const userData = res?.data;
+          setUser(userData);
+          openToast(res.message, 'success');
+        }
+      } else {
+        const res = await updateUserProfile(data);
+
+        if (res?.code === 200) {
+          if (res?.data) {
+            const userData = res.data;
+            setUser(userData);
+          }
+          openToast(res.message, 'success');
+        }
+      }
+    } catch (error) {
+      const errorMessage = handleAxiosError(error);
+      openToast(errorMessage, 'error');
     }
   };
 
@@ -48,7 +73,7 @@ const EditProfilePage = () => {
         className="flex flex-col gap-2 w-full"
         onSubmit={profileForm.handleSubmit(
           handleSubmitChange as SubmitHandler<
-            z.infer<typeof userInfoVerificationSchema>
+            z.infer<typeof profileEditVerificationSchema>
           >,
         )}
       >
@@ -60,9 +85,17 @@ const EditProfilePage = () => {
 
         <Input
           control={profileForm.control}
-          name="nickname"
+          name="nickName"
           label="닉네임"
           placeholder="닉네임을 입력해주세요"
+          type="text"
+        />
+
+        <Input
+          control={profileForm.control}
+          name="accountNumber"
+          label="계좌번호"
+          placeholder="계좌번호를 입력해주세요"
           type="text"
         />
 
