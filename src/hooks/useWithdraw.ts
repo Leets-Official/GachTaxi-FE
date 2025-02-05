c471254751d0bf4d86ba713efb80093434b4ec50
김지원 <jiwonkim0531@gachon.ac.kr>
2025-02-05 12:36:44 +0900

    feat: 회원 탈퇴하기 api 연결

--- src/hooks/useWithdraw.ts
+++ src/hooks/useWithdraw.ts
@@ -1,19 +1,22 @@
 import { useNavigate } from 'react-router-dom';
 import { useToast } from '@/contexts/ToastContext';
 import { withdrawUser } from '@/libs/apis/withdrawUser.api';
+import useUserStore from '@/store/useUserStore';
 
 const useWithdraw = () => {
   const navigate = useNavigate();
   const { openToast } = useToast();
+  const { clearUser } = useUserStore();
 
   const onWithdraw = async () => {
     try {
       const response = await withdrawUser();
 
-      if (response.ok) {
+      if (response.code === 200) {
         localStorage.removeItem('accessToken');
         localStorage.removeItem('refreshToken');
-        openToast('계정이 성공적으로 탈퇴되었습니다.');
+        clearUser();
+        openToast('계정이 성공적으로 탈퇴되었습니다.', 'success');
         navigate('/');
       } else {
         throw new Error('계정 탈퇴 실패');
