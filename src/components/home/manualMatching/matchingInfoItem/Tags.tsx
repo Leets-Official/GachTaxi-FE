ca0bd41a8baa9a8f00e666613be5b689e3f1e6f0
WooGi1020 <jungwk1020@gmail.com>
2025-02-08 15:23:53 +0900

    fix: 부가적인 요소 수정

--- src/components/home/manualMatching/matchingInfoItem/Tags.tsx
+++ src/components/home/manualMatching/matchingInfoItem/Tags.tsx
@@ -1,4 +1,11 @@
+import TAGS from '@/constants/tags.constant';
+
 const Tags = ({ tags }: { tags: string[] }) => {
+  const showLabel = (value: string) => {
+    const tag = TAGS.find((tag) => tag.value === value);
+    return tag ? tag.label : value;
+  };
+
   return (
     <div className="flex overflow-x-scroll scroll-hidden gap-2">
       {tags.map((tag) => {
@@ -7,7 +14,7 @@ const Tags = ({ tags }: { tags: string[] }) => {
             key={tag}
             className="text-assistive min-w-fit text-black font-medium px-3 py-1 rounded-full bg-primary"
           >
-            # {tag}
+            # {showLabel(tag)}
           </span>
         );
       })}
